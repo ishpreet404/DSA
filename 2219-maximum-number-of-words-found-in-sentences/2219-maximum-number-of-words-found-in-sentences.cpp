b1541c877828870cdee7bd705033a8df5ff5d600
ishpreet404 <145900339+ishpreet404@users.noreply.github.com>
2025-10-21 00:48:29 +0530

    Time: 0 ms (100%), Space: 12.9 MB (93.06%) - LeetHub

---
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int count=0;
+        int maxcount=0;
+        for(int i=0;i<sentences.size();i++){
+            for(int j=0;j<sentences[i].size();j++){
+                if(sentences[i][j]==' '){
+                    count++;
+                }
+            }maxcount=max(maxcount,count);
+            count=0;
+        }return maxcount+1;
+    }
+};
