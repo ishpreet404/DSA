c3044a6f60b195bbb3ad9b68e0a500d5a834bb79
ishpreet404 <145900339+ishpreet404@users.noreply.github.com>
2025-11-28 19:42:29 +0530

    Time: 0 ms (100%), Space: 27.3 MB (51.42%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    void reve(int i , int j, vector<char>& s){
-        if(i>=j) return ;
-        swap(s[i],s[j]);
-        reve(i+1,j-1,s);
+    void reve(int i , int n, vector<char>& s){
+        if(i>=n/2) return ;
+        swap(s[i],s[n-i-1]);
+        reve(i+1,n,s);
     }
     void reverseString(vector<char>& s) {
         int n = s.size();
-        reve(0,n-1,s);
+        reve(0,n,s);
     }
 };
