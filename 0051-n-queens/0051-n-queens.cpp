f7aad1fb7ed282f1d8167243e248e6999c184109
ishpreet404 <145900339+ishpreet404@users.noreply.github.com>
2025-12-10 21:23:23 +0530

    Time: 2 ms (74.21%), Space: 10.4 MB (44.3%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,54 +1,40 @@
 class Solution {
 public:
-    unordered_set<int> colu ;
-    unordered_set<int> diag ;
-    unordered_set<int> antidiag;
     vector<vector<string>> result;
     vector<vector<string>> solveNQueens(int n) {
         vector<string> board(n,string(n,'.'));
         solve(board,0,0,n);
         return result;
     }
-    // bool isValid(vector<string> board,int row,int col,int n){
-    //     for(int i=row-1;i>=0;i--){
-    //         if(board[i][col]=='Q') return false;
-    //     }
-    //     int j=col-1;
-    //     for(int i=row-1;i>=0;i--){
-    //         if(j>=0){
-    //         if(board[i][j]=='Q') return false;
-    //         j--;
-    //         }
-    //     }
-    //      j=col+1;
-    //     for(int i=row-1;i>=0;i--){
-    //          if(j<=n){
-    //         if(board[i][j]=='Q') return false;
-    //         j++;}
-    //     }return true;
-    // }
+    bool isValid(vector<string> &board,int row,int col,int n){
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q') return false;
+        }
+        int j=col-1;
+        for(int i=row-1;i>=0;i--){
+            if(j>=0){
+            if(board[i][j]=='Q') return false;
+            j--;
+            }
+        }
+         j=col+1;
+        for(int i=row-1;i>=0;i--){
+             if(j<=n){
+            if(board[i][j]=='Q') return false;
+            j++;}
+        }return true;
+    }
     void solve(vector<string> &board,int row,int col,int n){
         if(row>=n){
             result.push_back(board);
             return;
         }
         for(col=0;col<n;col++){
-            int diagel = row+col;
-            int antidiagel =row-col;
-
-            if(colu.find(col)!=colu.end() ||diag.find(diagel)!=diag.end()||antidiag.find(antidiagel)!=antidiag.end() ){
-                continue;
-            }
-                colu.insert(col);
-                diag.insert(row+col);
-                antidiag.insert(row-col);
+            if(isValid(board,row,col,n)){
             board[row][col]='Q';
             solve(board,row+1,col,n);
             board[row][col]='.';
-                colu.erase(col);
-                diag.erase(row+col);
-                antidiag.erase(row-col);
-            
+            }
 
         }
     }
