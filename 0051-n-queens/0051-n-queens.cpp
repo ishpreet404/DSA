99f4ec44b76ceefde37711c58c76100b6e5a7e33
ishpreet404 <145900339+ishpreet404@users.noreply.github.com>
2025-12-10 21:26:55 +0530

    Time: 8 ms (14.7%), Space: 12.3 MB (16.09%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -9,24 +9,7 @@ public:
         solve(board,0,0,n);
         return result;
     }
-    // bool isValid(vector<string> board,int row,int col,int n){
-    //     for(int i=row-1;i>=0;i--){
-    //         if(board[i][col]=='Q') return false;
-    //     }
-    //     int j=col-1;
-    //     for(int i=row-1;i>=0;i--){
-    //         if(j>=0){
-    //         if(board[i][j]=='Q') return false;
-    //         j--;
-    //         }
-    //     }
-    //      j=col+1;
-    //     for(int i=row-1;i>=0;i--){
-    //          if(j<=n){
-    //         if(board[i][j]=='Q') return false;
-    //         j++;}
-    //     }return true;
-    // }
+ 
     void solve(vector<string> &board,int row,int col,int n){
         if(row>=n){
             result.push_back(board);
