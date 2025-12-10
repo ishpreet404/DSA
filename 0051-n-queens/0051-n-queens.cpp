796ba87ae96aa2ea9e6e9ff195b1a9983199931a
ishpreet404 <145900339+ishpreet404@users.noreply.github.com>
2025-12-10 16:11:05 +0530

    Time: 47 ms (5.23%), Space: 54.9 MB (5.22%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<string>> result;
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n,string(n,'.'));
+        solve(board,0,0,n);
+        return result;
+    }
+    bool isValid(vector<string> board,int row,int col,int n){
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q') return false;
+        }
+        int j=col-1;
+        for(int i=row-1;i>=0;i--){
+            if(j>=0){
+            if(board[i][j]=='Q') return false;
+            j--;
+            }
+        }
+         j=col+1;
+        for(int i=row-1;i>=0;i--){
+             if(j<=n){
+            if(board[i][j]=='Q') return false;
+            j++;}
+        }return true;
+    }
+    void solve(vector<string> board,int row,int col,int n){
+        if(row>=n){
+            result.push_back(board);
+            return;
+        }
+        for(col=0;col<n;col++){
+            if(isValid(board,row,col,n)){
+            board[row][col]='Q';
+            solve(board,row+1,col,n);
+            board[row][col]='.';
+            }
+
+        }
+    }
+};
