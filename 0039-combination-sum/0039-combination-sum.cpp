0def02910944f1dc9df9396f8ac0079b418dede7
ishpreet404 <145900339+ishpreet404@users.noreply.github.com>
2026-02-16 21:58:45 +0530

    Time: 108 ms (5.12%), Space: 80.3 MB (5.74%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -3,12 +3,12 @@ public:
    vector<vector<int>>  answer ;
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-         genSubarray({}, 0, candidates.size(), answer,candidates,target);
+         genSubarray({}, 0, candidates.size(), answer,candidates,target,0);
          return answer;
     }
 
- void genSubarray(vector<int> temp,long long idx,int n,vector<vector<int>> &answer,vector<int> &nums,int target){
-    int sum = accumulate(begin(temp),end(temp),0);
+ void genSubarray(vector<int> temp,long long idx,int n,vector<vector<int>> &answer,vector<int> &nums,int target,int sum){
+    // int sum = accumulate(begin(temp),end(temp),0);
     if(sum>target){
         return;
     }
@@ -18,9 +18,11 @@ public:
     return;
 }
         temp.push_back(nums[idx]);
-        genSubarray(temp,idx,n,answer,nums,target);
+        sum+=nums[idx];
+        genSubarray(temp,idx,n,answer,nums,target,sum);
         temp.pop_back();
-         genSubarray(temp,idx+1,n,answer,nums,target);
+        sum-=nums[idx];
+         genSubarray(temp,idx+1,n,answer,nums,target,sum);
 
     }
 };
